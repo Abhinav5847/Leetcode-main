d3a45b64a89445a597beffd1eb324d7ead6316a0
Abhinav <abnvv07@gmail.com>
2026-01-04 13:13:08 +0530

    Time: 0 ms (100%), Space: 12.5 MB (11.04%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if len(word) < 3:
+            return False
+        vw = set("aeiouAEIOU")
+        hasvw = False
+        hasc = False 
+
+        for i in word:
+            if not i.isalnum():
+                return False
+            if i.isalpha():
+                if i in vw:
+                    hasvw = True
+                else:
+                    hasc = True 
+
+        return hasvw and hasc                    
+
+        
