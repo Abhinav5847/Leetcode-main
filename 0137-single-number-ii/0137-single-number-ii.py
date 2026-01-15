da07abab7def584389ad0f9c19d4f683f9cc3f93
Abhinav <abnvv07@gmail.com>
2026-01-15 16:56:02 +0530

    Time: 3168 ms (5.07%), Space: 13.4 MB (85.6%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = [i for i in nums if nums.count(i) == 1]
+        return res[0]
+             
+           
+        
