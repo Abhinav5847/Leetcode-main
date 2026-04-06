dcb01f01c6734fdb5d7e9373bf1d23a5ee502ca7
Abhinav <abnvv07@gmail.com>
2026-04-06 12:14:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (67.85%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -4,14 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # wd = s.strip()
-        # if len(wd) >= 1:
-        #    res = wd.split(" ")
-        #    final = [i for i in res if i.isalpha() or len(i) > 1]
-        #    print(final)
-        #    return len(final)
-        # else:
-        #     return 0
 
         res = s.split()
         
