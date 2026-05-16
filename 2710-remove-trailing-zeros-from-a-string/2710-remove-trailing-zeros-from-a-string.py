faa39e0e82d0bf0bf9a3f19dcc7bde2e247ea4b5
Abhinav V <abnvv07@gmail.com>
2026-05-16 16:26:20 +0530

    Time: 23 ms (18.12%), Space: 12.4 MB (56.52%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeTrailingZeros(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+
+        for i in reversed(num):
+            if i == "0":
+                num = num[:-1]
+            else:
+                return num            
+
+
+
+        
