1cca755764b4cb6945bebb50ed76f134f54af5dd
Abhinav V <abnvv07@gmail.com>
2026-06-20 14:30:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.25%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        for i in s:
+
+            if s.count("A") >= 2:
+                return False
+
+            if "LLL" in s:
+                return False
+
+        return True             
+
+
+        
