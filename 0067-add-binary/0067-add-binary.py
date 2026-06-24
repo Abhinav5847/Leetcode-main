e0b58a2a5c3f189bac9202dfdbbd4e58d89d91a1
Abhinav V <abnvv07@gmail.com>
2026-06-24 15:07:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (59.01%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+
+        res = int(a,2) + int(b,2)
+
+        final = bin(res)
+
+        return final[2:]
+
+        
+
+
+        
