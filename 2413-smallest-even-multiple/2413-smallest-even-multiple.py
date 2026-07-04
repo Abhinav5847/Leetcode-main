b847dbbc400d87866a82d222087269ddec95105a
Abhinav V <abnvv07@gmail.com>
2026-07-04 14:15:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.07%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        i = 2
+
+        while True:
+
+            if i % 2 == 0 and i % n == 0:
+
+                return i
+
+            i += 2       
+
+       
+
+        
