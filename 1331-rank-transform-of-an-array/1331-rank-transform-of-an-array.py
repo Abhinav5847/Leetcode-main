3ee770f2b6d33a84ffe069c82eef3d6b93007351
Abhinav V <abnvv07@gmail.com>
2026-07-22 14:19:11 +0530

    Time: 430 ms (5.21%), Space: 27.9 MB (92.86%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -15,21 +15,14 @@ class Solution(object):
 
         res = []
 
+        print(numbers)
+
         for i in arr:
             res.append(numbers[i])
 
         return res        
         
-        # print(sorted_arr)
-        # index_nums = []
-
-        # for i in arr:
-
-        #     index = sorted_arr.index(i) + 1
-
-        #     index_nums.append(index)
-
-        # return index_nums    
+   
         
 
 
